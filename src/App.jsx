0c9497c85467741e9d08d9446e0d786133f5448b
punane <kadikerner@gmail.com>
2022-10-12 01:18:34 +0300

    visual

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import "./App.css";
 
 const API_URL = "https://dummyjson.com/";
 
@@ -14,20 +15,83 @@ function App() {
   }, []);
 
   const addToCart = (productId) => {
+    fetch(`https://dummyjson.com/carts/${productId}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        products: [
+          {
+            id: productId,
+          },
+        ],
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+      });
     console.log("Add to cart", productId);
     // Add to cart request: https://dummyjson.com/docs/carts#update
   };
 
   return (
     <div>
-      {products.map((product) => (
-        <div key={product.id}>
-          {product.title}{" "}
-          <button className="font-bold" onClick={() => addToCart(product.id)}>
-            Lisa ostukorvi
-          </button>
-        </div>
-      ))}
+      <div className="product__container">
+        {products.map((product) => (
+          <div className="card " key={product.id}>
+            <div className="card__image">
+              {product.discountPercentage && (
+                <div className="card__discountPercentage">
+                  -{product.discountPercentage.toFixed()}%
+                </div>
+              )}
+              <img
+                className="card__image--picture"
+                src={product.thumbnail}
+                alt="product image"
+              >
+                {products.thumbnail}
+              </img>
+            </div>
+            <div className="card__content">
+              <div className="card___content--left">
+                <h5 className="card__brand">{product.brand}</h5>
+                <h5 className="card__title">{product.title}</h5>
+                {product.status === 404 && <div>HELLO</div>}
+                <button
+                  // <CardBody className={`${!isUnlocked && 'blur-2px'}`}>
+
+                  className={`card__button card__button`}
+                  onClick={() => addToCart(product.id)}
+                >
+                  Lisa ostukorvi
+                </button>
+                {/* <div className="font-bold">Toode lisatud ostukorvi</div> */}
+              </div>
+              <div className="card__content--right">
+                {product.discountPercentage ? (
+                  <div className="card__price__container">
+                    <p className="card__price">
+                      {(
+                        product.price -
+                        product.price * (product.discountPercentage / 100)
+                      ).toFixed(2)}{" "}
+                      €
+                      <br />
+                      <span className="card__price--hint">*soodushind</span>
+                    </p>
+                    <p className="card__price card__price--discount">
+                      {product.price} €
+                    </p>
+                  </div>
+                ) : (
+                  <p className="card__price">{product.price} €</p>
+                )}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
